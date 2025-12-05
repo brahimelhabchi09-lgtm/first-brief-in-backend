6d73cf0d3df456c31299211bac08cc5e999c0db1
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2025-12-05 17:15:09 +0100

    the controllers of my website

---
+++ controllers/templates/footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-gray-800 text-white p-4 text-center mt-10">
+     2025 NovaCraft Studio
+</footer>
+
+</body>
+</html>
---
+++ controllers/templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $pageTitle ?></title>
+    <link href="./style/output.css" rel="stylesheet">
+</head>
+<body>
+
+<header class="bg-gray-900 text-white p-5">
+    <nav class="flex gap-6">
+        <a href="?page=home">Home</a>
+        <a href="?page=about">About</a>
+        <a href="?page=services">Services</a>
+        <a href="?page=contact">Contact</a>
+    </nav>
+</header>
